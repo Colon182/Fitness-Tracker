82b2fde41fa94c44852c9102b6858df4178869d2
Christian Colon <christian.colon27@gmail.com>
2021-09-28 15:24:31 -0400

    filled out sets in workout model

--- models/workout.js
+++ models/workout.js
@@ -40,7 +40,7 @@ const workoutSchema = new Schema(
         },
         sets: {
           // TODO: For the schema of this attribute, define type as number
-          // type: Number,
+          type: Number,
         },
         distance: {
           // TODO: For the schema of this attribute, define type as number
